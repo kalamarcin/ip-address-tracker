14ba2d30fb382df7672d9ec21459d78c96d3fcee
Marcin Kala <kalamarcin1@gmail.com>
2022-12-19 10:32:27 +0100

    async

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
   const handleSearch = () => {
     fetchData()
   }
-  const fetchData = async () => {
+  const fetchData =  () => {
     const APIKEY = process.env.REACT_APP_UNSPLASH_KEY
     const API = `https://geo.ipify.org/api/v2/country,city?apiKey=${APIKEY}&ipAddress=${searchInput.current.value}`
 
-    await axios
+     axios
       .get(API)
       .then(res => {
         setIpAdress(res.data.ip)
